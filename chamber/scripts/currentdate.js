80944fdf81a8a56264e72d9216e4ff2943216a0d
Tayballan1991 <bal22009@byui.edu>
2022-10-07 19:35:05 -0600

    Added in current date to header

--- chamber/scripts/currentdate.js
+++ chamber/scripts/currentdate.js
@@ -1,1 +1,11 @@
-document.getElementById("year").innerHTML = new Date().getFullYear()
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
